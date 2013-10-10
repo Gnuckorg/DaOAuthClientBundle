a6d06f4c9569f84443043a0b6610e2ad5f5bb7f6
Gnucki <tprelot@gmail.com>
2013-10-10 11:56:13 +0200

    doctrine:schema conflict

--- Entity/FosUser.php
+++ Entity/FosUser.php
@@ -1,86 +1,89 @@
 <?php
 
-namespace Da\OAuthClientBundle\Entity;
+// TODO: set this class in another bundle to remove the bad behaviour on doctrine:schema:update which
+// ask for the FOSUserBundle.
 
-use Symfony\Component\Security\Core\User\UserInterface;
-use FOS\UserBundle\Model\User as BaseUser;
-use Doctrine\ORM\Mapping as ORM;
-use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
-use Da\OAuthClientBundle\Security\Core\User\OAuthUserInterface;
+// namespace Da\OAuthClientBundle\Entity;
 
-/**
- * User is the user implementation used by the fosub user provider.
-*
- * @ORM\Entity
- * @ORM\Table(name="User")
- */
-class FosUser extends BaseUser implements OAuthUserInterface
-{
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
+// use Symfony\Component\Security\Core\User\UserInterface;
+// use FOS\UserBundle\Model\User as BaseUser;
+// use Doctrine\ORM\Mapping as ORM;
+// use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+// use Da\OAuthClientBundle\Security\Core\User\OAuthUserInterface;
 
-    /**
-     * Construct
-     */
-    public function __construct()
-    {
-        parent::__construct();
+// /**
+//  * User is the user implementation used by the fosub user provider.
+//  *
+//  * @ORM\Entity
+//  * @ORM\Table(name="User")
+//  */
+// class FosUser extends BaseUser implements OAuthUserInterface
+// {
+//     /**
+//      * @ORM\Id
+//      * @ORM\Column(type="integer")
+//      * @ORM\GeneratedValue(strategy="AUTO")
+//      */
+//     protected $id;
 
-        // Dumb unused password.
-        $this->password = base_convert(sha1(uniqid(mt_rand(), true)), 16, 36);
-        $this->enabled = true;
-    }
+//     /**
+//      * Construct
+//      */
+//     public function __construct()
+//     {
+//         parent::__construct();
 
-    /**
-     * {@inheritDoc}
-     */
-    public function setFromResponse(UserResponseInterface $response)
-    {
-        $this->setUsername($response->getNickname());
-		$this->setEmail($response->getEmail());
-    }
+//         // Dumb unused password.
+//         $this->password = base_convert(sha1(uniqid(mt_rand(), true)), 16, 36);
+//         $this->enabled = true;
+//     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function getRoles()
-    {
-        return array('ROLE_USER', 'ROLE_OAUTH_USER');
-    }
+//     /**
+//      * {@inheritDoc}
+//      */
+//     public function setFromResponse(UserResponseInterface $response)
+//     {
+//         $this->setUsername($response->getNickname());
+// 		$this->setEmail($response->getEmail());
+//     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function getPassword()
-    {
-        return null;
-    }
+//     /**
+//      * {@inheritDoc}
+//      */
+//     public function getRoles()
+//     {
+//         return array('ROLE_USER', 'ROLE_OAUTH_USER');
+//     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function getSalt()
-    {
-        return null;
-    }
+//     /**
+//      * {@inheritDoc}
+//      */
+//     public function getPassword()
+//     {
+//         return null;
+//     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function eraseCredentials()
-    {
-        return true;
-    }
+//     /**
+//      * {@inheritDoc}
+//      */
+//     public function getSalt()
+//     {
+//         return null;
+//     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function equals(UserInterface $user)
-    {
-        return $user->getUsername() === $this->username;
-    }
-}
+//     /**
+//      * {@inheritDoc}
+//      */
+//     public function eraseCredentials()
+//     {
+//         return true;
+//     }
+
+//     /**
+//      * {@inheritDoc}
+//      */
+//     public function equals(UserInterface $user)
+//     {
+//         return $user->getUsername() === $this->username;
+//     }
+// }
