d4c341e02c6e83e5a7fa2369ee2b0cd933391eb7
Gnucki <tprelot@gmail.com>
2014-04-04 16:59:24 +0200

    Decouple DaApiClientBundle from DaAuthCommonBundle

--- Event/ExceptionListener.php
+++ Event/ExceptionListener.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Da\AuthCommonBundle\Exception\ApiHttpResponseException;
 use Da\AuthCommonBundle\Security\AuthorizationRefresherInterface;
 
 /**
@@ -34,7 +33,10 @@ class ExceptionListener
      * @param HttpKernelInterface             $kernel                 The HTTP kernel.
      * @param AuthorizationRefresherInterface $authorizationRefresher The authorization refresher.
      */
-    public function __construct(HttpKernelInterface $kernel, AuthorizationRefresherInterface $authorizationRefresher)
+    public function __construct(
+        HttpKernelInterface $kernel,
+        AuthorizationRefresherInterface $authorizationRefresher
+    )
     {
         $this->kernel = $kernel;
         $this->authorizationRefresher = $authorizationRefresher;
@@ -44,18 +46,32 @@ class ExceptionListener
     {
         $exception =  $event->getException();
 
-        if ($exception instanceof ApiHttpResponseException) {
-            if (401 === $exception->getStatusCode() && $exception->isFirstTry()) {
-                // Retry the request after refreshing the authorization.
-                // Master request because we need to reload the user.
-                $this->authorizationRefresher->refresh();
-                $response = $this->kernel->handle($event->getRequest());
-            } else {
-                $response = new Response();
-                $response->setStatusCode($exception->getStatusCode());
-                $response->headers->replace($exception->getHeaders());
-                $response->headers->set('Content-Type', 'application/json');
-                $response->setContent($exception->getJsonMessage());
+        if ($exception instanceof HttpExceptionInterface) {
+            $headers = $exception->getHeaders();
+            $request = $event->getRequest();
+            
+            if (isset($headers['X-Da-Agent'])) {
+                $try = $request->headers->get('X-Request-Try', 0);
+
+                if (401 === $exception->getStatusCode() && 0 <= $try) {
+                    $request->headers->set('X-Request-Try', $try + 1);
+
+                    // Retry the request after refreshing the authorization.
+                    // Master request because we need to reload the user.
+                    $this->authorizationRefresher->refresh();
+                    $response = $this->kernel->handle($event->getRequest());
+                } else {
+                    $response = new Response();
+                    $response->setStatusCode(502);
+                    $response->headers->set('Content-Type', 'application/json');
+
+                    $options = 0;
+                    if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+                        $options = JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT;
+                    }
+
+                    $response->setContent(json_encode($headers, $options));
+                }
             }
 
             $event->setResponse($response);
